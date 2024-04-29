e335a30ffa2706529f14cc878803ead130311759
Dimitris C <dchatzieleftheriou@blockchain.com>
2024-04-29 19:49:13 +0300

    Adds Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobileIntelligence",
-            url: "https://github.com/dchatzieleftheriou-bc/swift-sardine/releases/download/v1.0.31/MobileIntelligence.xcframework.zip",
+            url: "https://github.com/dchatzieleftheriou-bc/swift-sardine/releases/download/1.0.31/MobileIntelligence.xcframework.zip",
             checksum: "74f1ad9b89e6a03d6eee476a3bf87bb7b146f0aab418334d11a2e6ed26388d32"
         )
     ]
